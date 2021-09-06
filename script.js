328fa77b70c4f7f1c48f29e8a2a9650a6f5142a6
FeLaar <57942217+FeLaar@users.noreply.github.com>
2021-09-06 13:23:10 +0200

    les1

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var x = 50;
+var y = 50;
+
+var speedX = 2;
+var speedY = 1;
 /**
  * setup
  * de code in deze functie wordt één keer uitgevoerd door
@@ -8,7 +13,7 @@ function setup() {
   createCanvas(1280, 720);
 
   // Kleur de achtergrond blauw, zodat je het kunt zien
-  background('blue');
+ 
 }
 
 
@@ -18,9 +23,25 @@ function setup() {
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
 function draw() {
+// Kleur de achtergrond blauw, zodat je het kunt zien
+  background('blue');
+
   // stel vulkleur in
-  fill(100, 100, 255);
+  fill(255, 255, 255);
 
   // teken een cirkel
-  ellipse(50,50,80,80);
+  ellipse(x,y,80,80);
+
+  x = x + speedX;
+  y = y + speedY;
+
+if (y === 680){
+  speedY = speedY * -1
+};
+
+if (x === 1240){
+ speedX = speedX *-1
+}
+
+
 }
